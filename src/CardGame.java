cb3b255ca91dafe64862d5bff4dab4f7ba73e679
JayBains <jayarjunbains@gmail.com>
2025-02-12 09:32:20 +0000

    Added constructor for CardGame and deckOfCards - Added method to list deckOfCards

--- src/CardGame.java
+++ src/CardGame.java
@@ -1,12 +1,28 @@
 import java.util.ArrayList;
 
 public class CardGame {
-    private String name;
-    public ArrayList<Card> deckOfCards = new ArrayList<>();
+    public ArrayList<Card> deckOfCards;
+
+    public CardGame() {
+        this.deckOfCards = new ArrayList<>();
+        createDeck();
+    }
 
     private void createDeck(){
         String[] symbols = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
         String[] suits = {"♥", "♣", "♦", "♠"};
-        int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+        int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+        for (String suit : suits) {
+            for (int i = 0; i < symbols.length; i++){
+                deckOfCards.add(new Card(suit, symbols[i], values[i]));
+            }
+        }
+    }
+
+    public void listDeck(){
+        for (Card card : deckOfCards){
+            System.out.println(card);
+        }
     }
 }
