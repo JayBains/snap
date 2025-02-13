96d9efb7d923909640c88d53e7272b3c5c403719
JayBains <jayarjunbains@gmail.com>
2025-02-13 16:53:25 +0000

    Created class for Snap that extends CardGame - Created method for Snap - instantiated variables in Snap

---
+++ src/Snap.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Snap extends CardGame{
+
+    public void playGame(){
+        Scanner myObj = new Scanner(System.in);
+        String previousCard = null;
+        boolean gameOver = false;
+
+        System.out.println("Game started. Press enter key to draw a card.");
+
+    }
+}
