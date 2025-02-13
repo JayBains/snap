3773def1b1e64e1e1a96aac7c252a4139d3a0e81
JayBains <jayarjunbains@gmail.com>
2025-02-13 18:41:38 +0000

    Created game loop - game loop catches errors

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+
         Snap newGame = new Snap("Jay's snap");
-        System.out.println(newGame.getDeck());
         newGame.shuffleDeck();
-        System.out.println(newGame.getDeck());
-        newGame.sortDeckInNumberOrder();
-        System.out.println(newGame.getDeck());
-        newGame.sortDeckIntoSuits();
-        System.out.println(newGame.getDeck());
-        newGame.shuffleDeck();
-        System.out.println(newGame.getDeck());
-        newGame.dealCard();
-        newGame.dealCard();
-        newGame.dealCard();
-        newGame.dealCard();
-        newGame.dealCard();
-        System.out.println(newGame.getDeck());
+        newGame.playGame();
     }
 }
--- src/Snap.java
+++ src/Snap.java
@@ -1,17 +1,34 @@
+import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Snap extends CardGame{
-
     public Snap(String name) {
         super(name);
     }
 
+
     public void playGame(){
-        Scanner myObj = new Scanner(System.in);
-        String previousCard = null;
+        Scanner scannerObj = new Scanner(System.in);
+        String previousCardSymbol = null;
         boolean gameOver = false;
 
         System.out.println("Game started. Press enter key to draw a card.");
 
+        while (!gameOver) {
+            try {
+                scannerObj.nextLine();
+                Card currentCard = dealCard();
+                if (Objects.equals(currentCard.getSymbol(), previousCardSymbol)){
+                    System.out.println("SNAP!");
+                    gameOver = true;
+                } else {
+                    previousCardSymbol = currentCard.getSymbol();
+                }
+            } catch(NoSuchElementException e) {
+                System.out.println(e.getMessage());
+                gameOver = true;
+            }
+        }
     }
 }
