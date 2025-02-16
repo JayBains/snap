fe346e815a8d34af2b7304319a89575897657d65
JayBains <jayarjunbains@gmail.com>
2025-02-16 01:14:59 +0000

    Created Player objects - added input reader for number of players

--- src/Snap.java
+++ src/Snap.java
@@ -7,28 +7,48 @@ public class Snap extends CardGame{
         super(name);
     }
 
+    Player playerOne = new Player();
+    Player playerTwo = new Player();
 
     public void playGame(){
         Scanner scannerObj = new Scanner(System.in);
         String previousCardSymbol = null;
         boolean gameOver = false;
+        boolean playerOneTurn = false;
 
-        System.out.println("Game started. Press enter key to draw a card.");
+        System.out.println("How many players are playing? (1 or 2)");
+        int input = Integer.parseInt(scannerObj.nextLine());
 
-        while (!gameOver) {
-            try {
-                scannerObj.nextLine();
-                Card currentCard = dealCard();
-                if (Objects.equals(currentCard.getSymbol(), previousCardSymbol)){
-                    System.out.println("SNAP!");
+        if (input == 1) {
+
+            System.out.println("Game started. Press enter key to draw a card.");
+            while (!gameOver) {
+                try {
+                    scannerObj.nextLine();
+                    Card currentCard = dealCard();
+                    if (Objects.equals(currentCard.getSymbol(), previousCardSymbol)){
+                        System.out.println("SNAP!");
+                        gameOver = true;
+                    } else {
+                        previousCardSymbol = currentCard.getSymbol();
+                    }
+                } catch(NoSuchElementException e) {
+                    System.out.println(e.getMessage());
                     gameOver = true;
-                } else {
-                    previousCardSymbol = currentCard.getSymbol();
                 }
-            } catch(NoSuchElementException e) {
-                System.out.println(e.getMessage());
-                gameOver = true;
             }
+
+        } else if (input == 2){
+
+            System.out.println("Player One please enter name: ");
+            playerOne.setName(scannerObj.nextLine());
+
+            System.out.println("Player One please enter name: ");
+            playerTwo.setName(scannerObj.nextLine());
+
+            System.out.println(playerOne.getName());
+            System.out.println(playerTwo.getName());
+
         }
     }
 }
