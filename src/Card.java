ad86835e5ed91f24b8c3913ad81cf6bb099e8487
JayBains <jayarjunbains@gmail.com>
2025-02-16 01:13:01 +0000

    Removed unused import - made variables final

--- src/Card.java
+++ src/Card.java
@@ -1,9 +1,7 @@
-import java.util.Comparator;
-
 public class Card {
-    private String suit;
-    private String symbol;
-    private int value;
+    private final String suit;
+    private final String symbol;
+    private final int value;
 
     public Card(String suit, String symbol, int value) {
         this.suit = suit;
