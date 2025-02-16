dc6030d81573441a1f4b61e74f9d851ecec87a29
JayBains <jayarjunbains@gmail.com>
2025-02-16 01:32:41 +0000

    Added ticker to make code more efficient - system prints name of players whos turn it is

--- src/Snap.java
+++ src/Snap.java
@@ -14,7 +14,7 @@ public class Snap extends CardGame{
         Scanner scannerObj = new Scanner(System.in);
         String previousCardSymbol = null;
         boolean gameOver = false;
-        boolean playerOneTurn = true;
+        int playerTurn = 1;
 
         System.out.println("How many players are playing? (1 or 2)");
         int input = Integer.parseInt(scannerObj.nextLine());
@@ -43,43 +43,39 @@ public class Snap extends CardGame{
             System.out.println("Player One please enter name: ");
             playerOne.setName(scannerObj.nextLine());
 
-            System.out.println("Player One please enter name: ");
+            System.out.println("Player Two please enter name: ");
             playerTwo.setName(scannerObj.nextLine());
 
             System.out.println(playerOne.getName() + " verses " + playerTwo.getName() + ".");
             System.out.println("Game started. Press enter key to draw a card.");
+            System.out.println(playerOne.getName() + "'s turn.");
 
             while (!gameOver) {
-                if (playerOneTurn) {
-                    try {
-                        scannerObj.nextLine();
-                        Card currentCard = dealCard();
-                        if (Objects.equals(currentCard.getSymbol(), previousCardSymbol)) {
+                try {
+                    scannerObj.nextLine();
+                    Card currentCard = dealCard();
+
+                    if (Objects.equals(currentCard.getSymbol(), previousCardSymbol)) {
+                        if (playerTurn == 1) {
                             System.out.println("SNAP! " + playerOne.getName() + " wins.");
-                            gameOver = true;
                         } else {
-                            previousCardSymbol = currentCard.getSymbol();
-                            playerOneTurn = false;
-                        }
-                    } catch (NoSuchElementException e) {
-                        System.out.println(e.getMessage());
-                        gameOver = true;
-                    }
-                } else {
-                    try {
-                        scannerObj.nextLine();
-                        Card currentCard = dealCard();
-                        if (Objects.equals(currentCard.getSymbol(), previousCardSymbol)) {
                             System.out.println("SNAP! " + playerTwo.getName() + " wins.");
-                            gameOver = true;
-                        } else {
-                            previousCardSymbol = currentCard.getSymbol();
-                            playerOneTurn = true;
                         }
-                    } catch (NoSuchElementException e) {
-                        System.out.println(e.getMessage());
                         gameOver = true;
+                    } else {
+                        previousCardSymbol = currentCard.getSymbol();
                     }
+                } catch (NoSuchElementException e) {
+                    System.out.println(e.getMessage());
+                    gameOver = true;
+                }
+
+                if (playerTurn == 1){
+                    System.out.println(playerTwo.getName() + "'s turn.");
+                    playerTurn++;
+                } else {
+                    System.out.println(playerOne.getName() + "'s turn.");
+                    playerTurn--;
                 }
             }
         }
