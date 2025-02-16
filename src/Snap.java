8292e8147bf84e0c3aba54ad89c502fed4efdb89
JayBains <jayarjunbains@gmail.com>
2025-02-16 01:22:15 +0000

    Added two player capability in snap game

--- src/Snap.java
+++ src/Snap.java
@@ -14,7 +14,7 @@ public class Snap extends CardGame{
         Scanner scannerObj = new Scanner(System.in);
         String previousCardSymbol = null;
         boolean gameOver = false;
-        boolean playerOneTurn = false;
+        boolean playerOneTurn = true;
 
         System.out.println("How many players are playing? (1 or 2)");
         int input = Integer.parseInt(scannerObj.nextLine());
@@ -46,9 +46,42 @@ public class Snap extends CardGame{
             System.out.println("Player One please enter name: ");
             playerTwo.setName(scannerObj.nextLine());
 
-            System.out.println(playerOne.getName());
-            System.out.println(playerTwo.getName());
+            System.out.println(playerOne.getName() + " verses " + playerTwo.getName() + ".");
+            System.out.println("Game started. Press enter key to draw a card.");
 
+            while (!gameOver) {
+                if (playerOneTurn) {
+                    try {
+                        scannerObj.nextLine();
+                        Card currentCard = dealCard();
+                        if (Objects.equals(currentCard.getSymbol(), previousCardSymbol)) {
+                            System.out.println("SNAP! " + playerOne.getName() + " wins.");
+                            gameOver = true;
+                        } else {
+                            previousCardSymbol = currentCard.getSymbol();
+                            playerOneTurn = false;
+                        }
+                    } catch (NoSuchElementException e) {
+                        System.out.println(e.getMessage());
+                        gameOver = true;
+                    }
+                } else {
+                    try {
+                        scannerObj.nextLine();
+                        Card currentCard = dealCard();
+                        if (Objects.equals(currentCard.getSymbol(), previousCardSymbol)) {
+                            System.out.println("SNAP! " + playerTwo.getName() + " wins.");
+                            gameOver = true;
+                        } else {
+                            previousCardSymbol = currentCard.getSymbol();
+                            playerOneTurn = true;
+                        }
+                    } catch (NoSuchElementException e) {
+                        System.out.println(e.getMessage());
+                        gameOver = true;
+                    }
+                }
+            }
         }
     }
 }
